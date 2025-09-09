16681597072119aa2e77a2c09be18de48fe3586e
ainunkhankholah <ainunkhankholah@gmail.com>
2025-09-09 08:44:39 +0700

    update: models-fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'features',
+        'technologies',
+        'impact',
+        'screenshot',
+        'url'
+    ];
 }
